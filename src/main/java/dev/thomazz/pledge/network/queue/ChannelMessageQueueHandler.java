a64bcf6016a72a0a2bf840de520fc5c1162e7682
ThomasOM <55256088+ThomasOM@users.noreply.github.com>
2024-04-01 20:37:38 -0600

    Use queued message to prevent netty memory leak

--- src/main/java/dev/thomazz/pledge/network/queue/ChannelMessageQueueHandler.java
+++ src/main/java/dev/thomazz/pledge/network/queue/ChannelMessageQueueHandler.java
@@ -4,6 +4,7 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelOutboundHandlerAdapter;
 import io.netty.channel.ChannelPromise;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.util.Deque;
@@ -12,21 +13,19 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 @Setter
 @Getter
 public class ChannelMessageQueueHandler extends ChannelOutboundHandlerAdapter {
-    private final Deque<Object> messageQueue = new ConcurrentLinkedDeque<>();
+    private final Deque<QueuedMessage> messageQueue = new ConcurrentLinkedDeque<>();
     private QueueMode mode = QueueMode.PASS;
 
     @Override
     public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
         switch (this.mode) {
-            default:
             case ADD_FIRST:
-                this.messageQueue.addFirst(msg);
-                promise.setSuccess();
+                this.messageQueue.addFirst(new QueuedMessage(msg, promise));
                 break;
             case ADD_LAST:
-                this.messageQueue.addLast(msg);
-                promise.setSuccess();
+                this.messageQueue.addLast(new QueuedMessage(msg, promise));
                 break;
+            default:
             case PASS:
                 super.write(ctx, msg, promise);
                 break;
@@ -40,8 +39,17 @@ public class ChannelMessageQueueHandler extends ChannelOutboundHandlerAdapter {
     }
 
     public void drain(ChannelHandlerContext ctx) {
-        this.messageQueue.forEach(ctx::write);
-        this.messageQueue.clear();
+        while (!this.messageQueue.isEmpty()) {
+            QueuedMessage message = this.messageQueue.poll();
+            ctx.write(message.message, message.promise);
+        }
+
         ctx.flush();
+    }
+
+    @RequiredArgsConstructor
+    private static class QueuedMessage {
+        private final Object message;
+        private final ChannelPromise promise;
     }
 }
